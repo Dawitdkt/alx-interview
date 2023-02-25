7a21e0ff22b88511416a5531f24c63050b385da9
Dawitdkt <dawitkinfu5@gmail.com>
2023-02-25 03:10:19 +0300

    all done

---
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,37 @@
+def validUTF8(data):
+    # Initialize a variable to store the number of bytes to process
+    n_bytes = 0
+
+    # Iterate over each integer in the data list
+    for num in data:
+
+        # Get the binary representation of the least significant 8 bits
+        bin_rep = format(num, '#010b')[-8:]
+
+        # If this is the first byte
+        if n_bytes == 0:
+
+            # Count how many consecutive 1s are in the most significant bits
+            for bit in bin_rep:
+                if bit == '0':
+                    break
+                n_bytes += 1
+
+            # If there are no 1s or more than 4 1s, it is not a valid UTF-8 character
+            if n_bytes == 0:
+                continue
+            elif n_bytes == 1 or n_bytes > 4:
+                return False
+
+        # If this is a subsequent byte
+        else:
+
+            # Check if the first two bits are 10, otherwise it is not a valid UTF-8 character
+            if not (bin_rep[0] == '1' and bin_rep[1] == '0'):
+                return False
+
+        # Decrease the number of bytes to process by one
+        n_bytes -= 1
+
+    # Return True if there are no remaining bytes to process, otherwise False
+    return n_bytes == 0
