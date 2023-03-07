dcae2da381a24bf92d6f7592c354c64ec631ff49
Dawitdkt <dawitkinfu5@gmail.com>
2023-03-07 17:51:11 +0300

    semi fix

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -5,30 +5,30 @@ const movieId = process.argv[2];
 
 // Make a GET request to the Star Wars API films endpoint
 request(`https://swapi.dev/api/films/${movieId}`, (error, response, body) => {
-    if (error) {
-        console.error('Error:', error);
-    } else if (response.statusCode !== 200) {
-        console.error('Unexpected status code:', response.statusCode);
-    } else {
-        // Parse the JSON response
-        const data = JSON.parse(body);
+  if (error) {
+    console.error('Error:', error);
+  } else if (response.statusCode !== 200) {
+    console.error('Unexpected status code:', response.statusCode);
+  } else {
+    // Parse the JSON response
+    const data = JSON.parse(body);
 
-        // Get the list of character URLs from the response
-        const characterUrls = data.characters;
+    // Get the list of character URLs from the response
+    const characterUrls = data.characters;
 
-        // Iterate through the character URLs and make a GET request for each one
-        characterUrls.forEach((url) => {
-            request(url, (error, response, body) => {
-                if (error) {
-                    console.error('Error:', error);
-                } else if (response.statusCode !== 200) {
-                    console.error('Unexpected status code:', response.statusCode);
-                } else {
-                    // Parse the JSON response and print the character name
-                    const data = JSON.parse(body);
-                    console.log(data.name);
-                }
-            });
-        });
-    }
+    // Iterate through the character URLs and make a GET request for each one
+    characterUrls.forEach((url) => {
+      request(url, (error, response, body) => {
+        if (error) {
+          console.error('Error:', error);
+        } else if (response.statusCode !== 200) {
+          console.error('Unexpected status code:', response.statusCode);
+        } else {
+          // Parse the JSON response and print the character name
+          const data = JSON.parse(body);
+          console.log(data.name);
+        }
+      });
+    });
+  }
 });
